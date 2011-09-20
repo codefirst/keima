5925c783a1848250c46faf0b937c681319fda716
mzp <mzpppp@gmail.com>
2011-09-20 23:16:07 +0900

    css更新 refs #784

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.configure(function() {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
+    app.use(require('stylus').middleware({ src: __dirname + '/public' }));
     app.use(express.static(__dirname + '/public'));
 });
 
