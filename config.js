237b4e3f15403d6787af04843a3c75f60d545533
suer <suetsugu.r@gmail.com>
2012-08-26 18:26:02 +0900

    mongodb url settings by env

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.mongodb = 'mongodb://localhost/keimadb';
+exports.mongodb = process.env.MONGO_URL || 'mongodb://localhost/keimadb';
 exports.redis   = {
         host : 'localhost',
         port : 6379
