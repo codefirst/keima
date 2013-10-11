6457f13cc1b850cfca0bb107d1ab5fe874c1dbfd
mallowlabs <mallowlabs@gmail.com>
2013-10-12 02:42:56 +0900

    Added: support mongohq

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.mongodb = process.env.MONGO_URL || 'mongodb://localhost/keimadb';
+exports.mongodb = process.env.MONGO_URL || process.env.MONGOHQ_URL || 'mongodb://localhost/keimadb';
 exports.redis   = {
         host : 'localhost',
         port : 6379
